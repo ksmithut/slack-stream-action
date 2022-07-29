8eeb7929b828f2dc26433978cb952b6ef535d792
ksmithut <github@ksmithut.com>
2022-07-28 19:27:44 -0600

    ino

--- src/pre.js
+++ src/pre.js
@@ -16,7 +16,7 @@ export async function run () {
   const jobStatus = core.getInput('status', { required: true })
   const octokit = getOctokit(githubToken)
   const slack = new WebClient(slackToken)
-  core.notice(JSON.stringify(context, null, 2))
+  core.info(JSON.stringify(context, null, 2))
 
   // await octokit.rest.actions.getWorkflow({
   //   ...context.repo,
